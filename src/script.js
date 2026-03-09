69cbfa630c863f00c4b687504d0e9dc314e7985a
bitpunkxr <tony69elvis@gmail.com>
2026-03-09 11:44:37 +0100

    Finished options pannel

--- src/script.js
+++ src/script.js
@@ -9,3 +9,35 @@ menu.addEventListener('click', () => {
     toggleDisplay();
 });
 
+const filter1 = document.getElementById('filter1');
+const filter2 = document.getElementById('filter2');
+const filter3 = document.getElementById('filter3');
+const option1 = document.getElementById('location');
+const option2 = document.getElementById('categories');
+const option3 = document.getElementById('price');
+
+function toggleLocation() {
+   option1.style.display = "block";
+   option2.style.display = "none";
+   option3.style.display = "none";
+};
+function toggleCategories() {
+   option1.style.display = "none";
+   option2.style.display = "block";
+   option3.style.display = "none";
+};
+function togglePrice() {
+   option1.style.display = "none";
+   option2.style.display = "none";
+   option3.style.display = "block";
+};
+
+filter1.addEventListener('click', () => {
+    toggleLocation();
+});
+filter2.addEventListener('click', () => {
+    toggleCategories();
+});
+filter3.addEventListener('click', () => {
+    togglePrice();
+});

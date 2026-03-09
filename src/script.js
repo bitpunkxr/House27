c56439aa987cabf8b367796eac3415f9c4d207c3
bitpunkxr <tony69elvis@gmail.com>
2026-03-09 12:19:07 +0100

    Updated options pannel. v1 for stability

--- src/script.js
+++ src/script.js
@@ -15,21 +15,25 @@ const filter3 = document.getElementById('filter3');
 const option1 = document.getElementById('location');
 const option2 = document.getElementById('categories');
 const option3 = document.getElementById('price');
+const dispName = document.getElementById('filter');
 
 function toggleLocation() {
-   option1.style.display = "block";
+   option1.style.display = "flex";
    option2.style.display = "none";
    option3.style.display = "none";
+   dispName.innerHTML = "Best Properties Based On <em>Location</em>";
 };
 function toggleCategories() {
    option1.style.display = "none";
-   option2.style.display = "block";
+   option2.style.display = "flex";
    option3.style.display = "none";
+   dispName.innerHTML = "Best Properties Based On <em>Categories</em>";
 };
 function togglePrice() {
    option1.style.display = "none";
    option2.style.display = "none";
-   option3.style.display = "block";
+   option3.style.display = "flex";
+   dispName.innerHTML = "Best Properties Based On <em>Price</em>";
 };
 
 filter1.addEventListener('click', () => {

ae2fd6684ac85348ad2158000ac083f22c7fd6d2
bitpunkxr <tony69elvis@gmail.com>
2026-03-11 22:43:01 +0100

    Made updates for UI on desktop including font-size, navbar and about section

--- src/script.js
+++ src/script.js
@@ -3,6 +3,10 @@ const option = document.getElementById('full-menu');
 const filter1 = document.getElementById('filter1');
 const filter2 = document.getElementById('filter2');
 const filter3 = document.getElementById('filter3');
+const option1 = document.getElementById('location');
+const option2 = document.getElementById('categories');
+const option3 = document.getElementById('price');
+const dispName = document.getElementById('filter');
 
 filter1.style.backgroundColor = "var(--bg)";
 filter1.style.color = "var(--secondary)";

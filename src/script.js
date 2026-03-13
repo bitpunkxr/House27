4cb7d5a4e0b13bbd27bf047a368ecc0adbd017c8
bitpunkxr <tony69elvis@gmail.com>
2026-03-13 14:30:44 +0100

    Debugging footer string in script

--- src/script.js
+++ src/script.js
@@ -12,6 +12,7 @@ const cName = document.querySelector('.c-name');
 let nDate = new Date();
 let year = nDate.getFullYear();
 cName.innerHTML = '&copy; HOUSE 27 ' + year + '. All Rights Reserved';
+console.log(cName.length);
 
 filter1.style.backgroundColor = "var(--bg)";
 filter1.style.color = "var(--secondary)";

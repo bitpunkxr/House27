b30272a476f80523fee61df3472c1685335798a0
bitpunkxr <tony69elvis@gmail.com>
2026-03-16 13:21:31 +0100

    Updated form and header links as well as header UI and slight change in color pallet

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const menu = document.getElementById('ham-menu');
 const option = document.getElementById('full-menu');
+const header = document.querySelector('.header');
 const filter1 = document.getElementById('filter1');
 const filter2 = document.getElementById('filter2');
 const filter3 = document.getElementById('filter3');
@@ -12,11 +13,13 @@ const cName = document.querySelector('.c-name');
 let nDate = new Date();
 let year = nDate.getFullYear();
 cName.innerHTML = '&copy; ' + year + ' H<rd>O</rd>USE 27. All Rights Reserved';
+
 filter1.classList.add('active');
 
 function toggleDisplay() {
     menu.classList.toggle('fold');
     option.classList.toggle('show');
+    header.classList.toggle('active');
 };
 menu.addEventListener('click', () => {
     toggleDisplay();

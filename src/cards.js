a0ba14fdb4b1e9384c4139b2dbf8c3a47187f7ba
bitpunkxr <tony69elvis@gmail.com>
2026-03-25 11:21:01 +0100

    Started Working on projects page. Having slight issues with card script

---
+++ src/cards.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const briefCards = document.querySelectorAll('.brief-card');
+
+    briefCards.forEach(briefCards => {
+        const button = briefCards.querySelector('.secondary');
+        const fullCard = briefCards.querySelector('.full-card');
+
+        if (button && fullCard) {
+            button.addEventListener('click', () => {
+                fullCard.classList.toggle('active');
+            });
+        }
+    });
+});
